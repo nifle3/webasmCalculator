4556cf1d9c9cd8b72cca965db076352dbc9c1a38
nifle3 <kuper116rus.2005@gmail.com>
2023-09-08 21:53:18 +0300

    fixed server files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"runtime"
+	"strings"
 )
 
 const (
 	port = ":8080"
-	path = "D:\\webasm\\assets"
 )
 
 func main() {
+	var dir string
+
+	if len(os.Args) > 2 && (os.Args[1] == "--dir" || os.Args[1] == "-D") {
+		dir = os.Args[2]
+	} else {
+		dir, _ = os.Getwd()
+		if runtime.GOOS == "windows" {
+			dir = strings.TrimSuffix(dir, "\\cmd\\server")
+			dir = dir + "\\assets"
+		} else {
+			dir = strings.TrimSuffix(dir, "/cmd/server")
+			dir = dir + "/assets"
+		}
+	}
+
 	log.Printf("server is listening on port %s", port)
-	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(path))))
+	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(dir))))
 }
